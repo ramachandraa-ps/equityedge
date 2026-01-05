73454ba21960af28b48fa078b6b57efffe7b415f
Ramachandraa <ramtheking2005@gmail.com>
2026-01-05 21:08:27 +0530

    feat: add UI components barrel export

---
+++ components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button, buttonVariants, type ButtonProps } from "./button";
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card";
+export { Input, inputVariants, type InputProps } from "./input";
+export { Badge, badgeVariants, type BadgeProps } from "./badge";
+export { Skeleton, SkeletonCard, SkeletonText } from "./skeleton";
+export { Tooltip, EducationalTooltip } from "./tooltip";
+export { Modal, ModalFooter } from "./modal";
+export { Tabs, TabsList, TabsTrigger, TabsContent } from "./tabs";
