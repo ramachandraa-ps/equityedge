4a2703e22e4ea81a57bd0ef098fa83bdf875ef24
Ramachandraa <ramtheking2005@gmail.com>
2026-01-05 21:10:11 +0530

    feat: add app and marketing route group layouts

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "@/components/layout";
+import { Footer } from "@/components/layout";
+
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/components/layout";
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
